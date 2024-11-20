7ba4f2908bde871dac48ef6f0dfd7517f8702f3b
unknown <forlarhh@gmail.com>
2024-11-20 11:23:03 +0100

    Fixed flake8 issues

--- app/app.py
+++ app/app.py
@@ -7,7 +7,10 @@ cache = redis.Redis(host='redis', port=6379)
 @app.route('/')
 def hello_world():
     count = cache.incr('hits')
-    return f'Hello, DevOps World on Azure! This page has been viewed {count} times.'
+    return (
+    f'Hello, DevOps World on Azure! '
+    f'This page has been viewed {count} times.'
+)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
